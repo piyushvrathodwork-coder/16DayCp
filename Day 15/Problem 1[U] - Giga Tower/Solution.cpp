0089285ccd8ce9acb47ad0e0ed1272e792f6b326
piyushvrathodwork-coder <piyushvrathod.work@gmail.com>
2026-03-28 21:18:57 +0530

    Resolved 15.1

---
+++ Day 15/Problem 1[U] - Giga Tower/Solution.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    long long n;
+    cin >> n;
+    long long b = 0;
+    if (n == 0){
+        b = 8;
+        cout << 8 << endl;
+    }else if(n > 0){
+        for(int i = 0; i<=100;i++){
+            int x = n;
+            while(x > 0){
+                int c = x % 10;
+                if (c == 8 && b != 0){
+                    cout << b << endl;
+                    return 0;
+                }
+                x /= 10;
+            }
+            n++;
+            b++;
+        }
+    }else{
+        for(int i = 0; i<=100;i++){
+            int nn = abs(n);
+            while(nn > 0){
+                int c = nn % 10;
+                if (c == 8 && b != 0){
+                    cout << b << endl;
+                    return 0;
+                }
+                nn /= 10;
+            }
+            n++;
+            b++;
+        }
+    }
+}
